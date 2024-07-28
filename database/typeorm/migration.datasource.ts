9930627d93f29f7e150b78d9f949d6ca0fbd95b6
Pedro Henrique <contatophss11@gmail.com>
2024-07-28 15:53:32 -0300

    initial migration-datasource provider and user entity for initial migration test

--- database/typeorm/migration.datasource.ts
+++ database/typeorm/migration.datasource.ts
@@ -1,5 +1,14 @@
 import { DataSource } from "typeorm";
 
+/**
+ * Example of a simple typeorm datasource that provides database connection
+ * It is advisable to use environment variables when passing credentials
+ * This is just an example of what it would look like
+ * 
+ * Important!: use synchronize = FALSE if this is your main datasource
+ * or it will make ALTER TABLE on the database every time the app initialize, may cause unexpected errors!
+ **/
+
 const migrationDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
@@ -9,7 +18,7 @@ const migrationDataSource = new DataSource({
   database: "saas",
   synchronize: false,
   logging: true,
-  entities: [],
+  entities: ["src/persistence/entities/*.entity.ts"],
   migrations: ["database/typeorm/migrations/*.ts"],
   subscribers: [],
 });
