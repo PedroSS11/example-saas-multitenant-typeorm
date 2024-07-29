fa39a1045425dc41f9386c7205d5e3edbc63e793
Pedro Henrique <contatophss11@gmail.com>
2024-07-29 20:24:11 -0300

    feat: tenant creation and migrations working inside our scope

--- database/typeorm/migration.exec.ts
+++ database/typeorm/migration.exec.ts
@@ -1,22 +1,22 @@
-import Logger from "jet-logger";
-import { execSync } from "child_process";
-import fs from "fs";
-import dotenv from "dotenv";
-import { Tenant } from "../../src/persistence/entities/management/tenant.entity";
-const { createConnection } = require("typeorm");
-import mysql from "mysql2/promise";
+import Logger from 'jet-logger';
+import { execSync } from 'child_process';
+import fs from 'fs';
+import dotenv from 'dotenv';
+import { Tenant } from '../../src/persistence/entities/management/tenant.entity';
+const { createConnection } = require('typeorm');
+import mysql, { FieldPacket, QueryResult } from 'mysql2/promise';
 dotenv.config();
 
 const mainDbConfig = {
-  host: process.env.DATABASE_HOST || "localhost",
+  host: process.env.DATABASE_HOST || 'localhost',
   port: process.env.DATABASE_PORT || 3306,
-  user: process.env.DATABASE_USER || "root",
+  user: process.env.DATABASE_USER || 'root',
   password: process.env.DATABASE_PASSWORD || null,
-  database: process.env.MAIN_DATABASE || "management",
+  database: process.env.MAIN_DATABASE || 'management',
 };
 
 async function getDatabaseNames() {
-  const connection = await createConnection({
+  const connection = await mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
@@ -24,17 +24,22 @@ async function getDatabaseNames() {
     port: 3306,
   });
 
-  const result = await connection.query("SELECT * FROM tenant");
-  console.log(result);
+  // Execução da query
+  const [rows, fields]: [mysql.RowDataPacket[], mysql.FieldPacket[]] =
+    await connection.query('SELECT full_name FROM tenant');
+
+  // Fechamento da conexão
   await connection.end();
-  return result.map((row: Tenant) => row?.full_name);
+
+  // Mapeamento dos nomes dos bancos de dados
+  return rows.map((row: mysql.RowDataPacket) => row.full_name);
 }
 
 async function generateMigrations() {
   const dbNames = await getDatabaseNames();
 
   for (const dbName of dbNames) {
-    process.env.DB_NAME = "company_" + dbName;
+    process.env.DB_NAME = 'company_' + dbName;
 
     // Create a folder for each tenant with their migrations
     const migrationsDir = `database/typeorm/migrations/company_${dbName}`;
@@ -45,7 +50,7 @@ async function generateMigrations() {
     try {
       execSync(
         `npx ts-node -r tsconfig-paths/register ./node_modules/typeorm/cli.js migration:generate ${migrationsDir}/migration -d ./ormconfig-migration.js`,
-        { stdio: "inherit" }
+        { stdio: 'inherit' },
       );
     } catch (error) {
       Logger.err(`Error generating migration for database ${dbName}: ${error}`);
